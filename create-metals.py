35520c0ac9eb2288be75319fffb76ec97cb24ea8
Scott Moonen <smoonen@us.ibm.com>
2024-12-20 13:31:39 -0500

    vCenter IP

--- create-metals.py
+++ create-metals.py
@@ -27,6 +27,11 @@ sg_rules = [ { 'direction' : 'inbound', 'ip_version' : 'ipv4', 'protocol' : 'all
 sg_id = vpclib.create_or_retrieve_security_group(inventory.vpc_id, sg_rules, 'smoonen-sg-intravpc')
 print("sg_id = '%s'" % sg_id)
 
+# Create vCenter VNI
+vcenter_id = vpclib.create_or_retrieve_vni(inventory.mgmt_subnet_id, "smoonen-vni-vcenter", sg_id)
+print("vcenter_id = '%s'" % vcenter_id)
+print("vcenter_ip = '%s'" % vpclib.get_vni(vcenter_id)['ips'][0]['address'])
+
 # Create three hosts
 for host in ('host001', 'host002', 'host003') :
   # Create three VNIs; two for PCI interfaces and one for VLAN interface
